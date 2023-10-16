9d10017c850443d178936383a7e526c678739ee2
Johny Lúcio <59095834+JohnyLucio7@users.noreply.github.com>
2023-10-16 17:55:06 -0300

    Update

--- Assets/Scripts/UpperBodyIK.cs
+++ Assets/Scripts/UpperBodyIK.cs
@@ -8,6 +8,16 @@ public class UpperBodyIK : MonoBehaviour
     [Header("Final IK Modules")]
     [SerializeField] private LookAtIK m_headLookAtIK = default;
     [SerializeField] private LookAtIK m_bodyLookAtIK = default;
+    [SerializeField] private ArmIK m_leftAimIK = default;
+    [SerializeField] private ArmIK m_rightAimIK = default;
+    [SerializeField] private FullBodyBipedIK m_fbbIK = default;
+
+    [Header("LookAt Settings")]
+    [SerializeField] Transform m_camera = default;
+    [SerializeField] Transform m_headTarget = default;
+
+    [Header("Head Effector Settings")]
+    [SerializeField] Transform m_headEffector = default;
 
     #endregion
 
@@ -17,17 +27,25 @@ public class UpperBodyIK : MonoBehaviour
     {
         m_headLookAtIK.enabled = false;
         m_bodyLookAtIK.enabled = false;
+        m_leftAimIK.enabled = false;
+        m_rightAimIK.enabled = false;
+        m_fbbIK.enabled = false;
     }
 
     private void Update()
     {
         m_bodyLookAtIK.solver.FixTransforms();
         m_headLookAtIK.solver.FixTransforms();
+        m_fbbIK.solver.FixTransforms();
+        m_rightAimIK.solver.FixTransforms();
+        m_leftAimIK.solver.FixTransforms();
     }
 
     private void LateUpdate()
     {
         LookAtIKUpdate();
+        FBBIKUpdate();
+        ArmsIKUpdate();
     }
 
     #endregion
@@ -39,6 +57,21 @@ public class UpperBodyIK : MonoBehaviour
         m_bodyLookAtIK.solver.Update();
         m_headLookAtIK.solver.Update();
     }
+
+    private void ArmsIKUpdate()
+    {
+        m_rightAimIK.solver.Update();
+        m_rightAimIK.solver.Update();
+    }
+
+    private void FBBIKUpdate()
+    {
+        m_fbbIK.solver.Update();
+
+        m_camera.LookAt(m_headTarget);
+        m_headEffector.LookAt(m_headTarget);
+    }
+
 
     #endregion
 }
